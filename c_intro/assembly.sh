7bbe562c53d83b0843132c0aadeecc42390a1d70
samuel <samueleniola29@gmail.com>
2022-09-06 05:25:18 -0700

    i just created some  file

---
+++ c_intro/assembly.sh
@@ -0,0 +1,2 @@
+!#usr/bin/bash
+gcc -E hello.c -o hello.s
---
+++ c_intro/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define AGE 45
+/**
+* main - entry point
+*
+*@return int 0
+*/
+int main (void)
+{
+    printf("Hello world!\n");
+    printf("%i\n", AGE);
+    return (0);
+}

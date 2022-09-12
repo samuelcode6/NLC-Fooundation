6f9d296071bf0f3565907ed47aadcbf25419e5c2
samuel <samueleniola29@gmail.com>
2022-09-12 06:36:59 -0700

    new update

---
+++ c_intro/limits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+/*
+Minimum 
+And Maximum 
+Sizes Of Data Types*/
+
+int main() {
+    printf("The minimum value of int = %d\n", INT_MIN);
+    printf("The maximum value of int = %d\n", INT_MAX);
+    printf("The minimum value of char = %d\n", CHAR_MIN);
+    printf("The maximum value of char = %d\n", CHAR_MAX);
+    printf("The minimum value of float %f\n", FLT_MIN);
+    printf("The maximum value of float = %f\n", FLT_MAX);
+    printf("The minimum value of long = %ld\n", LONG_MIN);
+    printf("The maximum value of long = %ld\n", LONG_MAX);
+    printf("The minimum value of double = %f\n", DBL_MIN);
+    printf("The maximum value of double = %f\n", DBL_MAX);
+    printf("The minimum value of short = %d\n", SHRT_MIN);
+    printf("The maximum value of short = %d\n", SHRT_MAX);
+    return (0);
+}
--- c_intro/types_and_sizes.c
+++ c_intro/types_and_sizes.c
@@ -9,30 +9,10 @@
 * @copyright copyright (c) 2022
 */
 
-int add_two (int y, int x);
-
-int main()
-{
-    //char
-    char c = '1';
-    // Print 'char is 1byte'
-    printf("char is %c byte\n",c);
-    add_two(2, 3);
-    return (0);
+int add_two (int y, int x) {
+    return x + y ;
 }
-
-int char_type () 
-{
-    //char
-    char C = '1';
-    //print 'char is 1 byte'
-    printf("char is %c byte\n", C);
-    
-}
-
-int add_two (int y, int x)
-{
-    int result = y + x;
-    printf("%d\n", result);
-    return (result);
+int main() {
+    printf("Result is: %d", add_two);
+    return(0);
 }

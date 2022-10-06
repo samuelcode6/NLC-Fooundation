4f0ebde6901e3cc7b699e6eb42df69f5aeba9ba6
samuel <samueleniola29@gmail.com>
2022-10-06 06:57:41 -0700

    still on it

--- python_intro/python_project.py
+++ python_intro/python_project.py
@@ -6,5 +6,20 @@ m = "Programming is like building a multilingual puzzle"
 #digit format print
 digit = "{number},{sname}".format (number = 98, sname = "Battery street")
 
+#float
+a_float = 3.14159
+b_float = round(a_float,2)
 
-print(digit)
+#string format
+str = "{str}".format (str = "Nervelabs Innovative")
+print("{}".format(str))
+
+#string concatenation
+str1 = "Nervelabs "
+str2 = "Innovative"
+str3 = str1 + str2
+print("Welcome to {}!".format(str3))
+
+
+
+print(m, digit, b_float, sep ='\n')
